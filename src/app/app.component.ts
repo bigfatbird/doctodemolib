b3cbec9d94e1b03ab8bb29b0c7f1cdc729cdc715
Marco Dahms <dahmsdahmsdahms@gmail.com>
2025-03-14 13:07:51 +0100

    Add header, add footer, add search section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { PatientSearchComponent } from "./patient-search/patient-search.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, PatientSearchComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
